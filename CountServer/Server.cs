31f0b0c68c21e8484c3c522cc246f5ab7bcb664f
VorteX <VorteX@DESKTOP-UKS4OO9>
2026-06-21 03:23:17 +0300

    Добавьте файлы проекта.

---
+++ CountServer/Server.cs
@@ -0,0 +1,30 @@
+﻿namespace CountServer
+{
+    static internal class Server
+    {
+        private static int count = 0;
+        static SemaphoreSlim writeSemaphore = new SemaphoreSlim(1, 1);
+
+
+        public async static Task<int> GetCount()
+        {
+            await Task.Delay(1000);
+            return count;
+        }
+
+
+        public async static Task AddToCount(int value)
+        {
+            await writeSemaphore.WaitAsync();
+
+            await Task.Delay(2000);
+            // чтобы сервер не упал из-за переполнения
+            if (count + value > int.MaxValue)
+                count = int.MaxValue;
+            else
+                count += value;
+
+            writeSemaphore.Release();
+        }
+    }
+}
